0ab08e1035706b541bed2ed788556e59267d889a
TinfoilxD <TinfoilxD@gmail.com>
2015-04-04 10:28:18 -0500

    altered hidden attributes

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton)
+    {
+        
+        //TODO: Record User's Voice
+        recordLabel.hidden = false
+        stopButton.hidden = false
+    }
 
+    @IBAction func stopRecording(sender: UIButton)
+    {
+        recordLabel.hidden = true
+        stopButton.hidden = true
+    }
 }
 
