fd796a6bc6e9b690fc7aeb5de42192a9ecd275e0
TinfoilxD <TinfoilxD@gmail.com>
2015-04-06 19:17:31 -0500

    finished

--- Pitch Perfect/PlaySoundViewController.swift
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -13,6 +13,8 @@ class PlaySoundViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer!
     var receivedAudio:RecordedAudio!
+    var audioEngine: AVAudioEngine!
+    var audioFile: AVAudioFile!
     
     override func viewDidLoad()
     {
@@ -35,6 +37,8 @@ class PlaySoundViewController: UIViewController {
         
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathURL, error: nil)
         audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathURL, error: nil)
     }
 
     override func didReceiveMemoryWarning()
@@ -58,6 +62,35 @@ class PlaySoundViewController: UIViewController {
         audioPlayer.play()
     }
     
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+       playAudioWithVariablePitch(1000)
+    }
+    
+    
+    @IBAction func playDarthVaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    private func playAudioWithVariablePitch(pitch: Float)
+    {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        audioPlayerNode.play()
+    }
     
     @IBAction func stopAudio(sender: UIButton) {
         audioPlayer.stop()
